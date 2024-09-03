7f2ae5e57b1a7369322cdc627086460ef293190d
resugamer01 <resunottaken@gmail.com>
2024-09-03 16:00:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7374624965:AAEvUh89PgMXILZkrpbzKoKMEdhneb_KyhY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("minecraft7707")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("2020009772")
 
 
 
